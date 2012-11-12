1ec304b7dd81956008682dd59673946a5dab0a56
Christian Lees <c.lees@acfr.usyd.edu.au>
2012-11-12 11:18:48 +1100

    more auv-control

--- src/acfr/auv-control/main.c
+++ src/acfr/auv-control/main.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <bot_param/param_client.h>
 
-
 #include "pid.h"
 #include "timer.h"
 
@@ -17,9 +16,17 @@
 // set the delta T to 0.1s, 10Hz loop rate
 #define CONTROL_DT 0.1
 
+typedef enum
+{   DEPTH_MODE,
+    PITCH_MODE
+} depth_mode_t;
+
 typedef struct
 {
     double vx;
+    double depth;
+    double pitch;
+    depth_mode_t depth_mode;
 } command_t;
 
 typedef struct 
@@ -28,6 +35,9 @@ typedef struct
     
     // controller gains
     pid_gains_t gains_vel;
+    pid_gains_t gains_roll;
+    pid_gains_t gains_depth;
+    pid_gains_t gains_pitch;
     
     // Nav solution
     acfrlcm_auv_acfr_nav_t nav;
@@ -36,6 +46,7 @@ typedef struct
     
     // controller inputs
     command_t command;
+    pthread_mutex_t command_lock;
     
 } state_t;
 
@@ -59,6 +70,20 @@ int load_config(state_t *state, char *rootkey)
 
 	sprintf(key, "%s.velocity.sat", rootkey);
 	state->gains_vel.sat = bot_param_get_double_or_fail(param, key);
+
+    // Roll gains
+	memset(&state->gains_roll, 0, sizeof(pid_gains_t));
+	sprintf(key, "%s.roll.kp", rootkey);
+	state->gains_roll.kp = bot_param_get_double_or_fail(param, key);
+
+	sprintf(key, "%s.roll.ki", rootkey);
+	state->gains_roll.ki = bot_param_get_double_or_fail(param, key);
+
+	sprintf(key, "%s.roll.kd", rootkey);
+	state->gains_roll.kd = bot_param_get_double_or_fail(param, key);
+
+	sprintf(key, "%s.roll.sat", rootkey);
+	state->gains_roll.sat = bot_param_get_double_or_fail(param, key);
 
 
     return 1;
@@ -121,6 +146,10 @@ int main(int argc, char **argv)
     // locks etc
     pthread_mutex_init(&state.nav_lock, NULL);	
     state.nav_alive = 0;
+    memset(&state.nav, 0, sizeof(acfrlcm_auv_acfr_nav_t));
+    
+    pthread_mutex_init(&state.command_lock, NULL);	
+    memset(&state.command, 0, sizeof(command_t));
  
     // LCM callbacks
     acfrlcm_auv_acfr_nav_t_subscribe (state.lcm, "ACFR_NAV", &acfr_nav_callback, &state);      
@@ -131,28 +160,43 @@ int main(int argc, char **argv)
 	
 	double prop_rpm;
 	double top_rudder_angle, bottom_rudder_angle, port_plane_angle, starboard_plane_angle;
-	
+	double roll_offset;
+	double pitch, plane_angle;
 	
 	// main loop
 	while(!main_exit)
 	{
-	    // lock the nav data
+	    // lock the nav and command data
 	    pthread_mutex_lock(&state.nav_lock);
+	    pthread_mutex_lock(&state.command_lock);
 	
         // X Velocity
         prop_rpm = pid(&state.gains_vel, state.nav.vx, state.command.vx, CONTROL_DT);
-
-        // unlock the nav data
+        
+        // Roll compenstation
+        // We try to keep the AUV level, ie roll = 0
+        roll_offset = pid(&state.gains_roll, state.nav.roll, 0, CONTROL_DT);
+        
+        // Depth to pitch
+        pitch = pid(&state.gains_depth, state.nav.depth, state.command.depth, CONTROL_DT);
+        
+        // Pitch to fins
+        if(state.command.depth_mode = PITCH_MODE)
+            pitch = state.command.pitch;
+        plane_angle = pid(&state.gains_pitch, state.nav.pitch, pitch, CONTROL_DT);
+        
+        // unlock the nav and command data
         pthread_mutex_unlock(&state.nav_lock);
+        pthread_mutex_unlock(&state.command_lock);
         
         // send the motor command
         acfrlcm_auv_iver_motor_command_t mc;
         mc.utime = timestamp_now();
         mc.main = prop_rpm;
-        mc.top = 0;
-        mc.bottom = 0;
-        mc.port = 0;
-        mc.starboard = 0;
+        mc.top = 0 + roll_offset;
+        mc.bottom = 0 - roll_offset;
+        mc.port = 0 + roll_offset;
+        mc.starboard = 0 - roll_offset;
         mc.source = ACFRLCM_AUV_IVER_MOTOR_COMMAND_T_AUTO;
         acfrlcm_auv_iver_motor_command_t_publish(state.lcm, "IVER_MOTOR", &mc);
 
@@ -162,6 +206,7 @@ int main(int argc, char **argv)
 
     pthread_join(lcm_tid, NULL);
     pthread_mutex_destroy(&state.nav_lock);
+    pthread_mutex_destroy(&state.command_lock);
 	
 	return 1;
 }
