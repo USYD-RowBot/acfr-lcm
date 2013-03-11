3bb75df9471d9766d34fb3f76cc7e5ff8f649188
auv <auv@acfr.usyd.edu.au>
2013-03-11 23:18:48 +0000

    updates to acfr-cam-server

--- src/vision/acfr-cam-server/main.c
+++ src/vision/acfr-cam-server/main.c
@@ -134,7 +134,7 @@ int parse_message(char *data, state_t *state)
         sprintf(command, "mkdir -p %s\n", state->dir);
         system(command);
         system("killall acfr-cam-logger");
-        sprintf(command, "/home/auv/perls/bin/acfr-cam-logger -e PROSILICA_..16 -o %s &\n", state->dir);
+        sprintf(command, "/home/auv/git/acfr_lcm/build/bin/acfr-cam-logger -c PROSILICA_..16 -o %s &\n", state->dir);
         printf("Command: %s", command);
         system(command);
     
