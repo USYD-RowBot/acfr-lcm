aad629968933a0fe0b33b320fdfb2f49b57d96d2
Christian Lees <c.lees@acfr.usyd.edu.au>
2013-04-26 12:14:52 +1000

    fxed some simulation bugs, added some new ones

--- src/simulator/iver/test.cpp
+++ src/simulator/iver/test.cpp
@@ -1,16 +1,27 @@
 #include <small/Pose3D.hh>
 
+using namespace std;
+
 int main() {
     SMALL::Pose3D auvPose;
-    auvPose.setPosition(0,0,1);
-    auvPose.setRollPitchYawRad(0, 0, 0);
+    auvPose.setPosition(0.0,0.0,1.0);
+    auvPose.setRollPitchYawRad(0.0, 0.0, 0.0);
     
-    SMALL::Vector6D nu;
-    nu = 1.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    SMALL::Vector4D nu1, nu2;
+    nu1 = 1.0, 0.0, 0.0, 0.0;
+    nu2 = 0.0, 1.1, 0.0, 0.0;
     
-    SMALL::Vector6D eta_dot;
-    eta_dot = auvPose.transformTo(nu);
-    std::cout << eta_dot.toString() << std::endl;
+    
+   // cout << "rotation = " << auvPose.getRotationMatrix().toString() << endl;
+    
+        
+    SMALL::Vector3D eta_dot;
+    eta_dot = (auvPose.get3x4TransformationMatrix() * nu1);
+    cout << eta_dot.toString() << endl << endl;
+    
+    eta_dot = (auvPose.get3x4TransformationMatrix() * nu2);
+    cout << eta_dot.toString() << endl << endl;
+
     
     return 0;
 }
--- src/simulator/sim_test/auv_command1.py
+++ src/simulator/sim_test/auv_command1.py
@@ -4,7 +4,7 @@
 import lcm
 import sys
 
-sys.path.append('/home/clees/auv/acfr_lcm/build/lib/python2.6/dist-packages/perls/lcmtypes')
+sys.path.append('/home/clees/auv/git/acfr_lcm/build/lib/python2.7/dist-packages/perls/lcmtypes')
 
 from acfrlcm.auv_control_t import auv_control_t
 
@@ -14,7 +14,7 @@ msg = auv_control_t()
 
 msg.depth_mode = auv_control_t.DEPTH_MODE
 msg.run_mode = auv_control_t.RUN
-msg.heading = 1.57
+msg.heading = 0.0
 msg.depth = 10.0
 msg.altitude = 0.0
 msg.pitch = 0.0
