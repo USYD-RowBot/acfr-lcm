99cbe43cfbcb1686d7a337438b35318e86883aa8
Navid Nourani Vatani <n.nourani-vatani@acfr.usyd.edu.au>
2014-03-04 00:25:20 +1100

    created a test function

--- src/acfr/global-planner/main.cpp
+++ src/acfr/global-planner/main.cpp
@@ -1,21 +1,16 @@
-#include "global_planner.hpp"
-#include <signal.h>
-
-int mainExit;
-
-void signalHandler(int sigNum) {
-    // do a safe exit
-    mainExit = 1;
-}
+#include "LegPath.h"
 
 
-int main(int argv char **argv) {
-    // install the signal handler
-    mainExit = 0;
-    signal(SIGINT, signalHandler);
-
-    // create a global planner object and let it do its thing
-    GlobalPlanner gp;
-    gp.run();
+int main(int argc, char **argv) {
     
-    return 
+	LegPath p;
+
+	p.setPosition( 0, 0, 0 );
+	p.setHeadingDeg(45.);
+	p.setLength(100);
+	p.setDropDist(5.0);
+	p.setPitchDeg(5.);
+	p.calcPath();
+
+    return 0;
+}
