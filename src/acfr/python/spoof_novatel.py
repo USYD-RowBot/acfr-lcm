f23df2d90cc000654e9d7c15f6514e7628f7d899
Christian Lees <c.lees@acfr.usyd.edu.au>
2014-10-22 11:40:28 +1100

    added spoof_ysi.py

--- src/acfr/python/spoof_novatel.py
+++ src/acfr/python/spoof_novatel.py
@@ -10,6 +10,7 @@ LCMROOT='/home/auv/git/acfr_lcm'
 
 sys.path.append('{}/build/lib/python2.7/dist-packages/perls/lcmtypes'.format(LCMROOT))
 from senlcm import novatel_t, usbl_fix_t
+from acfrlcm import auv_status_t
 
 
 olon = 152.092212561092
@@ -26,10 +27,15 @@ rad = 100
 rad2 = 90
 
 msg2 = usbl_fix_t()
+msg3 = auv_status_t()
+
+count = 0
 
 while True:
     msg.utime = int(time.time() * 1000000)
     msg2.utime = int(time.time() * 1000000)
+    msg2.utime = int(time.time() * 1000000)
+    
     lat = rad * math.cos(math.radians(angle))     
     lon = rad * math.sin(math.radians(angle))
     h = (angle + 90.0) % 360.0
@@ -37,6 +43,7 @@ while True:
     msg.latitude = math.radians(olat + lat * inc )
     msg.longitude = math.radians(olon + lon * inc)
     msg.heading = math.radians(h)
+    msg.status = "INS_SOLUTION_GOOD"
     
     lon = rad2 * math.cos(math.radians(angle-45))     
     lat = rad2 * math.sin(math.radians(angle-45))
@@ -46,9 +53,18 @@ while True:
     msg2.longitude = math.radians(olon + lon * inc)
     msg2.accuracy = 10
     
-    
+    msg3.latitude = math.radians(olat + lat * inc )
+    msg3.longitude = math.radians(olon + lon * inc)
+    msg3.status = 1<<9;
+
     lc.publish("NOVATEL", msg.encode())
     lc.publish("USBL_FIX", msg2.encode())
+    
+    if count == 20:
+        lc.publish("AUV_STATUS", msg3.encode())
+        count = 0
+    else:
+        count = count + 1
     
     angle = angle + 0.5
     
