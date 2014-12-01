f2a48c9cf06ce61c87b7e42b22c6f8e35eedb9ce
Christian Lees <c.lees@acfr.usyd.edu.au>
2014-12-02 10:22:21 +1100

    updated tcm to support sockets interface

--- src/sensors/tcm/main.c
+++ src/sensors/tcm/main.c
@@ -4,10 +4,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <math.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+#include <libgen.h>
 
 #include "perls-common/timestamp.h"
-#include "perls-common/error.h"
-#include "perls-common/generic_sensor_driver.h"
+#include <bot_param/param_client.h>
+#include "perls-common/serial.h"
 
 #include "perls-lcmtypes/senlcm_tcm_t.h"
 
@@ -17,12 +22,7 @@
 
 #define DTOR 3.141592/180
 
-static int
-myopts (generic_sensor_driver_t *gsd)
-{
-    getopt_add_description (gsd->gopt, "PNI TCM Fieldforce compass driver.");
-    return 0;
-}
+enum {io_socket, io_serial};
 
 
 int parse_tcm(char *buf, senlcm_tcm_t *tcm)
@@ -107,7 +107,7 @@ int tcm_form_message(char *in, int data_len, char *out)
     
 
 
-int program_tcm(generic_sensor_driver_t *gsd)
+int program_tcm(int fd)
 {
     char data[128];
     char out[128];
@@ -126,7 +126,7 @@ int program_tcm(generic_sensor_driver_t *gsd)
 //    data[8] = kZAligned;
     len = tcm_form_message(data, 6, out);
 //    len = tcm_form_message(data, 9, out);
-    gsd_write(gsd, out, len);
+    write(fd, out, len);
     
     
     // set the acquisition mode
@@ -137,7 +137,7 @@ int program_tcm(generic_sensor_driver_t *gsd)
     float interval = update_rate;
     memcpy(&data[7], &interval, sizeof(float));
     len = tcm_form_message(data, 11, out);    
-    gsd_write(gsd, out, len);
+    write(fd, out, len);
 
     // set the filter mode
     double c4[4] = {4.6708657655334e-2, 4.5329134234467e-1, 4.5329134234467e-1, 4.6708657655334e-2};
@@ -156,14 +156,14 @@ int program_tcm(generic_sensor_driver_t *gsd)
         memcpy(&data[4], c8, sizeof(double)*filter_length);
     }
     len = tcm_form_message(data, sizeof(double)*filter_length + 4, out);
-    gsd_write(gsd, out, len);
+    write(fd, out, len);
 
 
     // start
     memset(data, 0, sizeof(data));
     data[0] = kStartIntervalMode;
     len = tcm_form_message(data, 1, out);    
-    gsd_write(gsd, out, len);
+    write(fd, out, len);
 
     return 1;
 }
@@ -182,99 +182,140 @@ main (int argc, char *argv[])
     // install the signal handler
     program_exit = 0;
     signal(SIGINT, signal_handler);
+
+    //Initalise LCM object - specReading
+    lcm_t *lcm = lcm_create(NULL);
                 
-    generic_sensor_driver_t *gsd = gsd_create (argc, argv, NULL, myopts);
-    gsd_launch (gsd);
-    gsd_noncanonical(gsd, 1, 1);
+    // Read the LCM config file
+    BotParam *param;
+	char rootkey[64];
+	char key[64];
+	
+    param = bot_param_new_from_server (lcm, 1);
     
-    gsd_reset_stats(gsd);
+    sprintf(rootkey, "sensors.%s", basename(argv[0]));
+
+    // read the config file
+    int io;
+	sprintf(key, "%s.io", rootkey);
+	char *io_str = bot_param_get_str_or_fail(param, key);
+    if(!strcmp(io_str, "serial"))
+        io = io_serial;
+    else if(!strcmp(io_str, "socket"))
+        io = io_socket;
+    
+    char *serial_dev, *parity;
+    int baud;
+    char *ip, *inet_port;
+    
+    if(io == io_serial)
+    {
+        sprintf(key, "%s.serial_dev", rootkey);
+        serial_dev = bot_param_get_str_or_fail(param, key);
+
+    	sprintf(key, "%s.baud", rootkey);
+	    baud = bot_param_get_int_or_fail(param, key);
+
+	    sprintf(key, "%s.parity", rootkey);
+	    parity = bot_param_get_str_or_fail(param, key);
+    }
+    
+    if(io == io_socket)
+    {
+        sprintf(key, "%s.ip", rootkey);
+        ip = bot_param_get_str_or_fail(param, key);
+
+        sprintf(key, "%s.port", rootkey);
+        inet_port = bot_param_get_str_or_fail(param, key);
+    }
+
+    // Open either the serial port or the socket
+    struct addrinfo hints, *spec_addr;
+    int fd;
+    if(io == io_serial)
+    {
+        fd = serial_open(serial_dev, serial_translate_speed(baud), serial_translate_parity(parity), 1);
+        if(fd < 0)
+        {
+            printf("Error opening port %s\n", serial_dev);
+            return 0;
+        }
+    }        
+    else if(io == io_socket)
+    {
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        getaddrinfo(ip, inet_port, &hints, &spec_addr);
+    	fd = socket(spec_addr->ai_family, spec_addr->ai_socktype, spec_addr->ai_protocol);
+        if(connect(fd, spec_addr->ai_addr, spec_addr->ai_addrlen) < 0) 
+        {
+	        printf("Could not connect to %s on port %s\n", ip, inet_port);
+    		return 1;
+        }
+    
+    }
     
     int len;
     char buf[256];
     int64_t timestamp;
     senlcm_tcm_t tcm;
     
-    program_tcm(gsd);
+    program_tcm(fd);
     
-    gsd_flush(gsd);
-    
-    int start_pos;
+    fd_set rfds;
     
     while(!program_exit)
     {
         memset(buf, 0, sizeof(buf));
-        start_pos = 0;
-    	do
-        {
-            //len = gsd_read(gsd, buf, sizeof(buf), &timestamp);
-            //look for a leading 0 that may be the start of a data frame
-            len = gsd_read(gsd, buf, 1, &timestamp);
-            if (len < 1) {
-                printf("ERROR: got len = %d bytes from gsd_read. Resetting connection in 1s.\n", len);
-                sleep(1);
-                gsd_update_stats (gsd, false);
-                gsd_flush(gsd);
-                gsd_destroy(gsd);
-                gsd = gsd_create (argc, argv, NULL, myopts);
-                gsd_launch (gsd);
-                gsd_noncanonical(gsd, 1, 1);
-            } else {
-                for(int i=0; i<len; i++)
-                    if(buf[i] == 0)
-                    {
-                        start_pos = i;
-                        break;
-                    }
-            }
-        } while(buf[start_pos] != 0 && len > 0);
-
-        if(len < 2)
-       	    len += gsd_read(gsd, &buf[start_pos + 1], 1, NULL);
-    	    
- 	    unsigned short data_len = (buf[start_pos] << 8) + buf[start_pos + 1];
-
-        // the minimum valid  data length is 5.  Make sure we don't pass an
-        // invalide length to the tcm_crc function.
-        if(data_len > 5)
-        {
         
-            // read the rest of the data
-
-            while(len < data_len)
-                len += gsd_read(gsd, &buf[start_pos + len], data_len - len, NULL);
-
-           	// check the checksum
-            unsigned short crc = *(unsigned short *)&buf[start_pos + len - 2];
-            
-            if(tcm_crc(&buf[start_pos], data_len-2) == (((crc >> 8) | (crc & 0xff) << 8)))
-            {
-                memset(&tcm, 0, sizeof(senlcm_tcm_t));
-                tcm.utime = timestamp;
-                // its good data, lets parse it
-
-                if(parse_tcm(&buf[start_pos], &tcm))
-                {
-                    senlcm_tcm_t_publish(gsd->lcm, gsd->channel, &tcm);
-                    gsd_update_stats (gsd, true);
-                }
-                else
-                    gsd_update_stats (gsd, false);
-            }
-	        else
-            {
-    	        printf("Bad CRC\n");
-                gsd_update_stats (gsd, false);
-            }
-        }
-	    else
+        FD_ZERO(&rfds);
+        FD_SET(fd, &rfds);
+	
+		struct timeval tv;
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+	    
+	    int ret = select (FD_SETSIZE, &rfds, NULL, NULL, &tv);
+        if(ret == -1)
+            perror("Select failure: ");
+        else if(ret != 0)
         {
-         printf("\nBad data_len %d\n", data_len);
-            gsd_update_stats (gsd, false);
-        }
-    
-    }
+            timestamp = timestamp_now();
+            len = read(fd, buf, 1);
+            if(len < 1 && buf[0] != 0)
+                continue;
+            else
+            {
+                // read another byte
+                len += read(fd, &buf[1], 1);
+                unsigned short data_len = (buf[0] << 8) + buf[1];   
+                if(data_len > 5)
+                {
+                    // read the rest of the data
+                    while(len < data_len)
+                        len += read(fd, &buf[len], data_len - len);
 
-    gsd_destroy(gsd);
+                    // check the checksum
+                    unsigned short crc = *(unsigned short *)&buf[len - 2];
+                    if(tcm_crc(&buf[0], data_len-2) == (((crc >> 8) | (crc & 0xff) << 8)))
+                    {
+                        memset(&tcm, 0, sizeof(senlcm_tcm_t));
+                        tcm.utime = timestamp;
+                        // its good data, lets parse it
+
+                        if(parse_tcm(&buf[0], &tcm))
+                            senlcm_tcm_t_publish(lcm, "TCM", &tcm);
+                    }
+        	        else
+            	        printf("Bad CRC\n");
+    	        }
+                printf("\nBad data_len %d\n", data_len);
+            }
+        }
+    }        
+        
+    close(fd);
     
     return 0;
     
