e25eea369b155196c424bc792f34b0297777ff65
auv@glider <auv@glider>
2015-03-27 14:00:11 +0000

    Updates on waveglider at ScottReef201503

--- src/sensors/evologics/evologics_usbl.cpp
+++ src/sensors/evologics/evologics_usbl.cpp
@@ -78,7 +78,7 @@ int Evologics_Usbl::process_usblfix(const std::string& channel, const evologics_
     SMALL::Vector3D target;
     target = ef->y, ef->x, ef->z;
     
-    cout << "Evologics_Usbl got local fix " << ef->y << " " << ef->x << " " << ef->z << endl;
+    cout << "Evologics_Usbl got local fix: x:" << ef->x << " y: " << ef->y << " z: " << ef->z << " n:" << ef->n << " e:" << ef->e << " r:" << ef->r << " p:" << ef->p << " h:" << ef->h << endl;
     
     SMALL::Pose3D ship;
     ship.setPosition(0, 0, 0);
@@ -159,8 +159,8 @@ int Evologics_Usbl::process_usblfix(const std::string& channel, const evologics_
     } 
     else if(gps_source == GPS_GPSD)
     {
-        ship_latitude = gpsd.fix.latitude;
-        ship_longitude = gpsd.fix.longitude;
+        ship_latitude = gpsd.fix.latitude * RTOD;
+        ship_longitude = gpsd.fix.longitude * RTOD;
     } 
      
     sprintf(proj_str, "+proj=tmerc +lon_0=%f +lat_0=%f +units=m", ship_longitude, ship_latitude);
@@ -176,11 +176,14 @@ int Evologics_Usbl::process_usblfix(const std::string& channel, const evologics_
     double y = (double)target_world[0];           
     pj_transform(pj_tmerc, pj_latlong, 1, 1, &x, &y, NULL);
 
+cout << "Ship lat: " << ship_latitude << " lon:" << ship_longitude << endl;
+cout << "Target lat: " << y << " lon:" << x << endl;
+
     usbl_fix_t uf;
     uf.utime = timestamp_now();
     uf.remote_id = ef->remote_id;
-    uf.latitude = y * DTOR;
-    uf.longitude = x * DTOR;
+    uf.latitude = y ;
+    uf.longitude = x ;
     uf.depth = target_world[2];
     uf.accuracy = ef->accuracy;
     uf.ship_longitude = ship_longitude * DTOR;
