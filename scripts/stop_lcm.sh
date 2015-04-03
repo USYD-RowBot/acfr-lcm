9795d537f9d38e554e551382dbaad38378be9171
Lachlan Toohey <l.toohey@acfr.usyd.edu.au>
2015-04-03 12:00:19 +0800

    Fixed stop_lcm.sh to use shortened name and check if killed/already killed.

--- scripts/stop_lcm.sh
+++ scripts/stop_lcm.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-pkill bot-procman-deputy
+
+pkill bot-procman-de
+
+result=$?
+
+if [ $result -eq 1 ]; then
+    echo "No deputy running."
+elif [ $result -eq 0 ]; then
+    echo "Killed deputy."
+else
+    echo "Unknown output of pkill ($result)"
+fi
