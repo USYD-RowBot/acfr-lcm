8ef96787faf089a34392335337ca93661114b1a6
auv <auv@katsonis>
2015-10-27 16:54:43 +1100

    Updated cam-trigger to reconnect

--- src/vision/cam-trigger/main-tcp.c
+++ src/vision/cam-trigger/main-tcp.c
@@ -259,6 +259,7 @@ void heartBeatHandler(const lcm_recv_buf_t *rbuf, const char *ch, const perllcm_
             printf("Detected broken pipe. Reconnecting.");
             close(state->triggerFd);
             triggerConnect(state);
+            brokenPipe = 0;
         }
 //		printf("KA = %d\n", state->keepAliveCount);
         if(state->keepAliveCount == 14)
