67710c26882fe7311f4f9ae16a71741bb64babd2
auv <auv@holt>
2016-09-19 01:14:21 +0000

    added mission stop command, CL

---
+++ missions/auv_task_stop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import lcm
+import sys
+
+sys.path.append('/usr/local/lib/python2.7/dist-packages/perls/lcmtypes')
+
+from acfrlcm.auv_global_planner_t import auv_global_planner_t
+
+lc = lcm.LCM();
+
+msg = auv_global_planner_t()
+msg.command = auv_global_planner_t.STOP
+    
+    
+lc.publish('TASK_PLANNER_COMMAND.IVERAME', msg.encode())
+
