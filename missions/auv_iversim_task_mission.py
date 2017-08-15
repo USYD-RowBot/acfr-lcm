b237d5aa89bdb29bcb260c373d16f369fff8ae54
Jorja <j.martin@acfr.usyd.edu.au>
2017-08-15 16:32:30 +1000

    Simulator Setup for Iver

---
+++ missions/auv_iversim_task_mission.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+import lcm
+import sys
+import xml.etree.ElementTree as ET
+
+sys.path.append('/usr/local/lib/python2.7/dist-packages/perls/lcmtypes')
+#sys.path.append('/usr/local/lib/python2.7/site-packages/perls/lcmtypes')
+
+from acfrlcm.auv_global_planner_t import auv_global_planner_t
+
+lc = lcm.LCM();
+
+f = open(sys.argv[1],"r")
+mission = f.readlines()
+missionXML = ET.fromstringlist(mission)
+missionString = ET.tostring(missionXML)
+
+msg = auv_global_planner_t()
+msg.command = auv_global_planner_t.GOTO
+msg.str = missionString
+    
+    
+lc.publish('TASK_PLANNER_COMMAND.iver-sim', msg.encode())
+
