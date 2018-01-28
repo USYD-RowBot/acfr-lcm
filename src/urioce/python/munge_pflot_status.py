8338a78a1755820ad9a046781a10af65b66fb8d6
Stefan Williams <stefan.williams@sydney.edu.au>
2018-01-28 20:54:19 +0000

    Changed order of message handling to reflect receipt of status followed by usbl_fix

--- src/urioce/python/munge_pflot_status.py
+++ src/urioce/python/munge_pflot_status.py
@@ -3,48 +3,54 @@
 
 import lcm
 import sys
+import math
 
 LCMROOT='/home/auv/git/acfr-lcm'
 
 sys.path.append('{}/build/lib/python2.7/dist-packages/perls/lcmtypes'.format(LCMROOT))
-from senlcm import usbl_fix_short_t
+from senlcm import usbl_fix_t
 from acfrlcm import auv_status_short_t
 from urioce_lcm import float_acomm_status_t
 
-lat = None 
-lon = None
+pfloatMsg = None
 
 def usblHandler(channel, data):
-    global lat, lon
-    msg = usbl_fix_short_t.decode(data)
+    global pfloatMsg
+    msg = usbl_fix_t.decode(data)
     print 'Received usb_fix_short_t on channel ' + channel
     
-    lat =  msg.latitude
+    lat = msg.latitude
     lon = msg.longitude
-
-
-def pfloatHandler(channel, data):
-    global lat, lon
-    pfloatMsg = float_acomm_status_t.decode(data)
-    print 'Received float_acomm_status_t on channel ' + channel
-
-    if lat is not None and lon is not None:
+    if pfloatMsg is not None:
 
         auvStatMsg = auv_status_short_t()
-        auvStatMsg.altitude = pfloatMsg.altitude
-        auvStatMsg.depth = pfloatMsg.depth
+        auvStatMsg.utime = pfloatMsg.timestamp * 1e6
+        if pfloatMsg.altitude < 1250:
+            auvStatMsg.altitude = pfloatMsg.altitude / 10
+        elif pfloatMsg.altitude > 12500:
+            auvStatMsg.altitude = -125
+        else:
+            auvStatMsg.altitude = -pfloatMsg.altitude / 100
+        auvStatMsg.depth = pfloatMsg.depth / 10
         auvStatMsg.charge = pfloatMsg.bat_charge
         auvStatMsg.waypoint = pfloatMsg.leg_num
-        auvStatMsg.latitude = lat
-        auvStatMsg.longitude = lon
+        auvStatMsg.latitude = lat*180/math.pi
+        auvStatMsg.longitude = lon*180/math.pi
         auvStatMsg.vel = 5
 
         lc.publish("PFLOAT.AUVSTAT", auvStatMsg.encode())
     else:
-        print 'Received pfloat status but waiting for usbl fix'
-    
+        print 'Received usbl_fix status but waiting for pfloat message'
+ 
+
+def pfloatHandler(channel, data):
+    global pfloatMsg
+    pfloatMsg = float_acomm_status_t.decode(data)
+    print 'Received float_acomm_status_t on channel ' + channel
+
+   
 lc = lcm.LCM()
-lc.subscribe("FALKOR.USBL_FIX.PFLOAT.", usblHandler)
+lc.subscribe("FALKOR.USBL_FIX.PFLOAT", usblHandler)
 lc.subscribe("AFS.PFLOAT", pfloatHandler)
 
 while True:
