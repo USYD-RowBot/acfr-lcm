e4c1c212a724571878f99da0e4087947f64fcdac
Jorja <j.martin@acfr.usyd.edu.au>
2018-02-05 11:59:46 -1000

    NavTranslate built untested

--- src/acfr/acfr-nav-translate/main.c
+++ src/acfr/acfr-nav-translate/main.c
@@ -6,10 +6,14 @@
 #include <errno.h>
 #include <string.h>
 #include <proj_api.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 
 #include <bot_param/param_client.h>
 #include "acfr-common/timestamp.h"
+#include "acfr-common/units.h"
 #include "perls-lcmtypes/acfrlcm_auv_acfr_nav_t.h"
 #include "perls-lcmtypes/senlcm_novatel_t.h"
 
@@ -27,6 +31,7 @@ typedef struct
 {
     lcm_t *lcm;
     char root_key[64];
+    bool verbose;
 	char *vehicle_name;
 	char ch_in_novatel[128]; // channel names (inc. vehicle prefix)
 	char ch_out_nav[128];
@@ -51,7 +56,7 @@ void incoming_msg_handler(const lcm_recv_buf_t *rbuf, const char *ch, const senl
 {
     state_t *state = (state_t *)u;
     acfrlcm_auv_acfr_nav_t nav;
-	memset(&nav_msg, 0, sizeof(acfrlcm_auv_acfr_nav_t));
+	memset(&nav, 0, sizeof(acfrlcm_auv_acfr_nav_t));
 
     // Translate
 	nav.utime = msg_in->utime;      	// keep orig. value 
@@ -60,69 +65,36 @@ void incoming_msg_handler(const lcm_recv_buf_t *rbuf, const char *ch, const senl
 	nav.longitude = msg_in->longitude;	// In (radians)
 	
 	// set to source F.O.R values, then translate to
-	nav.x = msg_in.latitude;			// Northings relative to origin (m)
-	nav.y = msg_in.longitude;			// Eastings relative to origin (m)
-	int error_code = pj_transform(this->gps, this->tmerc, 1, 1, &nav.x, &nav.y, 0);  // src_def, dest_def, point_count, point_offset, x, y, z 
+	nav.x = msg_in->latitude;			// Northings relative to origin (m)
+	nav.y = msg_in->longitude;			// Eastings relative to origin (m)
+	int error_code = pj_transform(state->gps, state->tmerc, 1, 1, &nav.x, &nav.y, 0);  // src_def, dest_def, point_count, point_offset, x, y, z 
 	if (error_code != 0)													 // x y z = N E D (z/D = null)
 	{
-		fprintf(stderr, "NavTranslate FATAL ERROR: can't perform Transverse Mercator Projection"\n);
-        return -1;
+		fprintf(stderr, "NavTranslate FATAL ERROR: can't perform Transverse Mercator Projection\n");
+        exit(-1);
 	}
     
 	nav.depth =	0;	 					            // could be det. from height - tides? but not used 
     nav.roll = msg_in->roll;		            	// Right-Handed Body F.O.R. (radians)
     nav.pitch = msg_in->pitch;	 		            // Right-Handed Body F.O.R. (radians)
-    nav.heading = msg_in->heading % (2 * M_PI);		// Right-Handed (+ down) Body F.O.R. (radians)
-    if (nav.heading < 0)    
+    nav.heading = fmod(msg_in->heading, (2 * M_PI));		// Right-Handed (+ down) Body F.O.R. (radians)
+    
+    double velocity_compass_angle = atan2(msg_in->east_velocity, msg_in->north_velocity); 
+    if (errno == EDOM) // No velocity case
     {
-        nav.heading += (2 * M_PI);
-    } 
-
-	double velocity_compass_angle = 0;
-	double velocity_magnitude = 0;
-    double velocity_body_angle = 0;
-	
-	// covering trig asymptote cases first
-	if (msg_in.east_velocity == 0)
-	{
-		if (msg_in.north_velocity >= 0)
-		{
-			velocity_compass_angle = 0;
-			velocity_magnitude = msg_in.north_velocity;
-		}
-		else
-		{
-			velocity_compass_angle = M_PI;
-			velocity_magnitude = -msg_in.north_velocity;
-		}
-	}
-	else if (msg_in.north_velocity == 0)
-	{
-		if (msg_in.east_velocity >= 0)
-		{
-			velocity_compass_angle = M_PI/2;
-			velocity_magnitude = msg_in.east_velocity;
-		}
-		else
-		{
-			velocity_compass_angle = (3 * M_PI)/2;
-			veocity_magnitude = -msg_in.east_velocity;
-		}
-	}
-	else // general cases
-	{
-		velocity_compass_angle = atan(msg_in.east_velocity/msg_in.north_velocity); 
-        if (velocity_compass_angle < 0)
-        {
-            velocity_compass_angle += (2 * PI);
-        }
-		velocity_magnitude = sqrt(msg_in.east_velocity * msg_in.east_velocity + msg_in.north_velocity * msg_in.north_velocity);
+        velocity_compass_angle = 0;
     }
-    velocity_body_angle = velocity_compass_angle - nav.heading; 
-  	nav.vx = velocity_magnitude * sin(velocity_body_angle);	// Body F.O.R. Forward velocity (m/s)
+    if (velocity_compass_angle < 0)
+    {
+        velocity_compass_angle += (2 * M_PI);
+    }
+	double velocity_magnitude = sqrt(msg_in->east_velocity * msg_in->east_velocity + msg_in->north_velocity * msg_in->north_velocity);
+    double velocity_body_angle = velocity_compass_angle - nav.heading; 
+  	
+    nav.vx = velocity_magnitude * sin(velocity_body_angle);	// Body F.O.R. Forward velocity (m/s)
    	nav.vy = velocity_magnitude * cos(velocity_body_angle);	// Body F.O.R. Starboard velocity (m/s)
     
-	nav.vz = -msg_in.up_velocity; 	    // not used for wam-v 2D planner  
+	nav.vz = -msg_in->up_velocity; 	    // not used for wam-v 2D planner  
 	nav.rollRate = 0;					// not used for wam-v 2D planner 
     nav.pitchRate = 0;					// not used for wam-v 2D planner 
     nav.headingRate = 0;				// not used for wam-v 2D planner 
@@ -130,7 +102,7 @@ void incoming_msg_handler(const lcm_recv_buf_t *rbuf, const char *ch, const senl
     nav.fwd_obstacle_dist = 0;			// not yet implemented
 
 
-    acfrlcm_asv_torqeedo_motor_status_t_publish(state->lcm, ch_out_nav, &nav_msg);    
+    acfrlcm_auv_acfr_nav_t_publish(state->lcm, state->ch_out_nav, &nav);    
 }
 
 
@@ -145,6 +117,7 @@ int main(int argc, char **argv)
     // Initialise state which holds all current data
     state_t state;
 	memset(&state.root_key, 0, 64);
+    state.verbose = false;
     
     // Initialise LCM
     state.lcm = lcm_create(NULL);
@@ -152,7 +125,6 @@ int main(int argc, char **argv)
 
     // Get vehicle name from command line arg
     char opt;
-    int got_key = 0;
     while((opt = getopt(argc, argv, "hk:n:")) != -1)
     {
         if(opt == 'h')
@@ -171,8 +143,8 @@ int main(int argc, char **argv)
     }
 	
 	// Set channel names with vehicle name
-	snprintf(ch_in_novatel, 128, "%s.NOVATEL", vehicle_name);
-	snprintf(ch_out_nav, 128, "%s.ACFR_NAV", vehicle_name);
+	snprintf(state.ch_in_novatel, 128, "%s.NOVATEL", state.vehicle_name);
+	snprintf(state.ch_out_nav, 128, "%s.ACFR_NAV", state.vehicle_name);
 
 	// Get the root key from the command line, program name
 	sprintf(state.root_key, "%s", basename(argv[0]));
@@ -187,33 +159,32 @@ int main(int argc, char **argv)
     }
 
 	// Read the config file values 
-    sprintf(key, "%s.verbose", root_key);
+    sprintf(key, "%s.verbose", state.root_key);
     printf("Requesting Parameter: %s\n", key);
     state.verbose = bot_param_get_boolean_or_fail(param, key);
 	
 	// And convert Lat and Long from degrees to radians
-    sprintf(key, "%s.origin_lat", root_key);
+    sprintf(key, "%s.origin_lat", state.root_key);
     printf("Requesting Parameter: %s\n", key);
-    state.origin[0] = DTOR * bot_param_get_TODO_or_fail(param, key);
+    state.origin[0] = DTOR * bot_param_get_double_or_fail(param, key);
 
-	printf(key, "%s.origin_long", root_key);
+	printf(key, "%s.origin_long", state.root_key);
     printf("Requesting Parameter: %s\n", key);
-    state.origin[1] = DTOR * bot_param_get_TODO_or_fail(param, key);
+    state.origin[1] = DTOR * bot_param_get_double_or_fail(param, key);
 	
 	// Setup for Transverse Mercator Projection using proj.4 library 
     char proj_str[128];
-    sprintf(proj_str, "+proj=tmerc +lat_0=%f +lon_0=%f +axis=ned +units=m", origin[0] * RTOD, origin[1] * RTOD);
+    sprintf(proj_str, "+proj=tmerc +lat_0=%f +lon_0=%f +axis=ned +units=m", state.origin[0] * RTOD, state.origin[1] * RTOD);
     state.gps = pj_init_plus("+proj=latlong +ellps=WGS84");
     state.tmerc = pj_init_plus(proj_str);
-	state.tmerc_origin = origin;
 	if (state.tmerc == 0 || state.gps == 0)
 	{
-		fprintf(stderr, "NavTranslate FATAL ERROR: can't setup Transverse Mercator Projection"\n);
+		fprintf(stderr, "NavTranslate FATAL ERROR: can't setup Transverse Mercator Projection\n");
 		return -1;
 	}
 
 	// Subscribe to the LCM channels for incoming messages
-    senlcm_novatel_t_subscribe(state.lcm, ch_in_novatel, &incoming_msg_handler, &state);
+    senlcm_novatel_t_subscribe(state.lcm, state.ch_in_novatel, &incoming_msg_handler, &state);
 
 	// Setup select for just the LCM msgs
     fd_set rfds, dup_rfds; // list of file descriptors where we will listen for incoming messages and duplicate set for use
@@ -221,6 +192,7 @@ int main(int argc, char **argv)
     struct timeval tv;
     FD_ZERO(&rfds);
     FD_SET(lcm_fd, &rfds); // add the lcm file descriptor to the set to watch
+    int ret = 0;
 
     // Main program loop
     while(!program_exit)
