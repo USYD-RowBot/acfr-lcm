d389a66537b4411cb3eeda88cf515ec248b29ee2
Lachlan Toohey <l.toohey@acfr.usyd.edu.au>
2018-03-22 11:18:43 +1100

    Adding colours to lcmdataplot

--- src/python/lcmplot/plotwindow.py
+++ src/python/lcmplot/plotwindow.py
@@ -2,6 +2,8 @@ from PyQt4.QtGui import QMainWindow
 from PyQt4.QtCore import Qt, pyqtSlot
 
 from window import Ui_DataPlotMain
+from datamodel import DataModel
+
 
 class PlotWindow(QMainWindow):
     def __init__(self, data):
@@ -13,18 +15,27 @@ class PlotWindow(QMainWindow):
 
         self.ui.dataTree.setModel(self.data)
 
+        self.ui.graphicsView.getPlotItem().addLegend()
+
         self.ui.pushAdd.clicked.connect(self.graph_addition_pressed)
         self.ui.pushRemove.clicked.connect(self.graph_removal_pressed)
+
+        n = 20
+        self.next_pen_idx = 0
+        self.pens = [(i, n) for i in xrange(n)]
+
 
     @pyqtSlot()
     def graph_addition_pressed(self):
         for idx in self.ui.dataTree.selectedIndexes():
-            name = self.data.data(idx, Qt.DisplayRole).toString()
-            data = self.data.data(idx, Qt.UserRole).toPyObject()
+            self.plot_data(self.data.data(idx, Qt.UserRole).toPyObject())
 
-            print name
+    def next_pen(self):
+        self.next_pen_idx += 1
+        return self.pens[self.next_pen_idx - 1]
 
-            self.ui.graphicsView.plot(data.time, data.data)
+    def plot_data(self, data):
+        self.ui.graphicsView.plot(data.time, data.data, pen=self.next_pen(), name=data.name)
 
     @pyqtSlot()
     def graph_removal_pressed(self):
