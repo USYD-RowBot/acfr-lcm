17d4a0b95ec7e4e0d10efc1c89399ed9405983ab
Christian Lees <c.lees@acfr.usyd.edu.au>
2018-07-03 10:30:13 +1000

    added timestamp function to acfr common lib

--- src/acfr/acfr-lcm-common/timestamp.c
+++ src/acfr/acfr-lcm-common/timestamp.c
@@ -152,3 +152,72 @@ timestamp_sync (timestamp_sync_state_t *s, int64_t dev_ticks,
     return dev_utime;
 }
 
+
+/* ----------------------------------------------------------------------
+
+   sprintfs dsl data time string based on computer system clock
+
+   MODIFICATION HISTORY
+   DATE         WHO             WHAT
+   -----------  --------------  ----------------------------
+   14 APR 1999  Louis Whitcomb  Created and Written
+   22 JAN 2001  SL              Renamed from rov_sprintf_dsl_time_string to
+                                rovtime2dsltime_str, added optional
+				time parameter, localtime->gmtime, 4-digit yr,
+				2-digit decimal seconds. If rov_time is < 0,
+				use current time
+   ---------------------------------------------------------------------- */
+int rovtime2dsltime_str(double rov_time, char *str)
+{ int    num_chars;
+
+  // for min, sec
+  double total_secs;
+  double secs_in_today;
+
+  //double day;
+  double hour;
+  double min;
+  double sec;
+
+  // for date and hours
+  struct tm  *tm;
+  time_t      current_time;
+
+  // read gettimeofday() clock and compute min, and
+  // sec with microsecond precision. Use rov_time if
+  // specified (rov_time >= 0)
+  if (rov_time >= 0) 
+    { total_secs = rov_time;
+      current_time = (time_t)total_secs;
+      tm = gmtime(&current_time);
+    }
+  else 
+    {//use current time
+      total_secs = ((double)(timestamp_now()))/1e6;
+      current_time = time(NULL);
+      tm = gmtime(&current_time);
+    }
+		
+  secs_in_today = fmod(total_secs,86400.0); //86400=24.0*60.0*60.0
+  hour = secs_in_today/3600.0;
+  min  = fmod(secs_in_today/60.0,60.0);
+  sec  = fmod(secs_in_today,60.0);
+
+  // call time() and gmtime for hour and date 
+  // current_time = time(NULL);
+  // tm = gmtime(&current_time);
+
+  num_chars = sprintf(str,"%04d/%02d/%02d %02d:%02d:%05.2f",
+		      (int) tm->tm_year+1900,
+		      (int) tm->tm_mon+1,
+		      (int) tm->tm_mday,
+		      (int) tm->tm_hour,
+		      (int) min,
+		      sec);
+
+   
+  return num_chars;
+}
+
+
+
