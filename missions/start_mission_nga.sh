1af052932bc8982248732e541cbc14c922c08c64
Lachlan Toohey <l.toohey@acfr.usyd.edu.au>
2019-01-31 13:26:24 +1100

    start_mission_nga.sh: add missing cam control start.

--- missions/start_mission_nga.sh
+++ missions/start_mission_nga.sh
@@ -19,12 +19,12 @@ export LCM_MISSION_DIR=$LOGDIR
 
 # kill any loggers that may be running
 killall -q lcm-logger
-killall -q acfr-cam-logger
 
 # copy the curent config file into the data directory
 cp /home/auv/git/acfr-lcm/config/nga.cfg $LOGDIR
 cp /home/auv/git/acfr-lcm/config/slam_nga.cfg $LOGDIR
 
 /home/auv/git/acfr-lcm/src/acfr/python/cam_control.py path $IMAGE_LOGDIR
+/home/auv/git/acfr-lcm/src/acfr/python/cam_control.py start
 lcm-logger -v -c "PROSILICA_..16" $LOGDIR/$MISSION_NAME.lcm > /dev/null 2> /dev/null &
 lcm-logger -v -c ".*RAW|PMD.*|.*STATS|PARAM.*|PROSILICA_..16|LCM.*|HEARTBEAT.*" $LOGDIR/${MISSION_NAME}_short.lcm > /dev/null 2> /dev/null &
