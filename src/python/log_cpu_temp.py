c65d4f716bceb69b049b01b5b617233b434431cc
auv <c.reeks@acfr.usyd.edu.au>
2019-03-04 13:32:07 +1100

    fixed temp logger to search for appropriate directory that stores coretemp

--- src/python/log_cpu_temp.py
+++ src/python/log_cpu_temp.py
@@ -3,21 +3,30 @@ import io
 import sys
 import lcm
 import string
+import os, os.path
 
 sys.path.append('/usr/local/lib/python2.7/dist-packages/perls/lcmtypes')
 
 from perllcm.heartbeat_t import heartbeat_t
 
 lc = lcm.LCM()
+# find the right dir that stores coretemp
+DIR = '/sys/class/hwmon/'
+available_dirs = os.listdir(DIR)
+for name in available_dirs:
+	with open('/sys/class/hwmon/'+name+'/name', 'r') as check_file:
+		if 'coretemp' in check_file.read():
+			right_dir = name
+			print 'coretemp stored here:/sys/class/hwmon/'+right_dir+'/temp1_input'
     
 def heartbeat_handler(channel, data):
 	hb = heartbeat_t.decode(data)
 
-	with open('/sys/class/hwmon/hwmon2/temp1_input', 'r') as temp_file:
+	with open('/sys/class/hwmon/'+right_dir+'/temp1_input', 'r') as temp_file:
 		current_temp = temp_file.read()
 
 	with open('temp_log.txt', 'a') as log_file:
-		log_file.write('{},{}\n'.format(hb.utime, current_temp))
+		log_file.write('{},{}'.format(hb.utime, current_temp))
 
 lc.subscribe("HEARTBEAT_1HZ", heartbeat_handler)
 
